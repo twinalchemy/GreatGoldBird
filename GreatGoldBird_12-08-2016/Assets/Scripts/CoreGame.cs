b5bb2c92029b0a5254fa266d738eed0c839e216f
Erik Blair <Erik Blair>
2017-02-23 22:45:59 -0500

    Camera Swap and scene assembly. ScreenFader Script. Bedroom Scene. Kitchen Scene. Lobby Scene. Need to add kitchen door to clickable objects. Need to import /create clickable object script(s)

---
+++ GreatGoldBird_12-08-2016/Assets/Scripts/CoreGame.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreGame : MonoBehaviour {
+
+
+    public int curView;
+
+    public Transform View1_Bedroom;
+    public Transform View2_Kitchen;
+
+    public bool isBedroom;
+
+    public Transform View3_Lobby;
+
+    public GameObject Inventory;
+    public GameObject DevConsole;
+
+    public GameObject Mailbox;
+
+
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.GetComponent<Transform>().SetParent(View1_Bedroom);
+        isBedroom = true;
+        curView = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftShift) || 
+            Input.GetKeyDown(KeyCode.RightShift) || 
+
+            (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightShift))) {
+
+                if(curView == 0) {
+                    Vector3 newPos = new Vector3(View2_Kitchen.transform.position.x,
+                                                    View2_Kitchen.transform.position.y,
+                                                    View2_Kitchen.transform.position.z - 10f);
+                    Camera.main.transform.position = newPos;
+                    curView = 1;
+                } else if(curView == 1) {
+                    Vector3 newPos = new Vector3(View1_Bedroom.transform.position.x,
+                                                        View1_Bedroom.transform.position.y,
+                                                        View1_Bedroom.transform.position.z - 10f);
+                    Camera.main.transform.position = newPos;
+                    curView = 0;
+                }
+            }
+
+        }
+	
+
+
+
+
+
+
+}
---
+++ GreatGoldBird_12-08-2016/Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,62 @@
+﻿//Author: William Thomas
+//Date: 04/12/2016
+//Purpose: To create and manage a fader prefab
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour {
+
+	public Texture2D aTexture;
+	public Color startColor;
+	public Color endColor;
+	private float fadeTime = 1.5f;
+	private float myTimer = 0.0f;
+
+	public delegate void FadeCompleteEvent ();
+
+	private event FadeCompleteEvent OnFadeComplete = null;
+
+	static public void createFade(string _prefabName, FadeCompleteEvent _completeEvent) {
+
+		Object fadePrefab = Resources.Load (_prefabName, typeof(Object));
+		GameObject fadeObject = Instantiate(fadePrefab) as GameObject;
+		ScreenFader fader = fadeObject.GetComponent<ScreenFader> ();
+		fader.OnFadeComplete = _completeEvent;
+
+	}
+
+
+
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		myTimer += Time.fixedDeltaTime;
+		if (myTimer > fadeTime) {
+			myTimer = fadeTime;
+			if (OnFadeComplete != null) {
+				OnFadeComplete ();
+				Destroy (gameObject, 0.1f);
+			}
+		}
+	}
+
+	void OnGUI() {
+		float percent = myTimer / fadeTime;
+		GUI.color = Color.Lerp (startColor, endColor, percent);
+		Rect screenRectangle = new Rect (0, 0, Screen.width, Screen.height);
+		GUI.DrawTexture (screenRectangle, aTexture);
+
+	}
+
+
+}
