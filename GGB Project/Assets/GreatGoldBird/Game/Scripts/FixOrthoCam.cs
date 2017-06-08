8585944e92ecb8ab9e2045a6ff5d96b1b997ccb0
garibaldy mukti <garibaldy.mukti@gmail.com>
2017-06-08 12:24:19 +0700

    update content

---
+++ GGB Project/Assets/GreatGoldBird/Game/Scripts/FixOrthoCam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FixOrthoCam : MonoBehaviour 
+{
+  public float horizontalResolution = 1920;
+
+  void OnGUI ()
+  {
+    float currentAspect = (float) Screen.width / (float) Screen.height;
+    this.GetComponent<Camera>().orthographicSize = horizontalResolution / currentAspect / 100;
+  }
+}
